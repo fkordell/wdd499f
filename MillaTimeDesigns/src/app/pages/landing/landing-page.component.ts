5e712c1f82603eb3f92446bcf195aeb18ede9302
Kordell Farley <far18009@byui.edu>
2024-12-05 11:53:11 -0700

    landing page styling and layout

--- MillaTimeDesigns/src/app/pages/landing/landing-page.component.ts
+++ MillaTimeDesigns/src/app/pages/landing/landing-page.component.ts
@@ -13,6 +13,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '@auth0/auth0-angular'; 
 import { UserContactService } from '../../services/user.service'; 
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-landing-page',
@@ -24,15 +28,17 @@ import { RouterModule } from '@angular/router';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit, OnDestroy {
   featuredProducts: Array<Product> | undefined;
+  gridCols: number = 4;
+  private breakPointSubscription!: Subscription;
   productSubscription: Subscription | undefined;
-  readonly fetchLimit = '20';
   emailForm: FormGroup;
 
   constructor(
@@ -40,7 +46,8 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     private cartService: CartService,
     private fb: FormBuilder,
     private userService: UserContactService, 
-    private authService: AuthService  
+    private authService: AuthService,
+    private breakPointObserver: BreakpointObserver  
   ) {
     this.emailForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
@@ -48,15 +55,33 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.loadFeaturedProducts();
+    this.breakPointSubscription = this.breakPointObserver
+      .observe([Breakpoints.Medium, Breakpoints.Large])
+      .pipe(
+        map((result) => {
+          console.log('Breakpoint result:', result.breakpoints);
+          if (result.breakpoints[Breakpoints.Medium]) {
+            console.log('Medium breakpoint');
+            this.gridCols = 4;
+          } else if (result.breakpoints[Breakpoints.Large]) {
+            console.log('Large breakpoint');
+            this.gridCols = 3;
+          } else {
+            console.log('Default breakpoint');
+            this.gridCols = 4;
+          }
+          return this.gridCols;
+        })
+      )
+      .subscribe((count) => {
+        this.loadFeaturedProducts(count);
+      });
   }
 
-  loadFeaturedProducts(): void {
-    this.productSubscription = this.storeService.getAllProducts(this.fetchLimit, 'desc')
-      .subscribe((products) => {
-        this.featuredProducts = this.getRandomProducts(products, 3);
-        console.log('Random Featured products:', this.featuredProducts);
-      });
+  loadFeaturedProducts(count: number): void {
+    this.productSubscription = this.storeService.getAllProducts('20', 'desc').subscribe((products) => {
+      this.featuredProducts = this.getRandomProducts(products, count);
+    });
   }
 
   getRandomProducts(products: Array<Product>, count: number): Array<Product> {
@@ -98,6 +123,9 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.productSubscription) {
       this.productSubscription.unsubscribe();
+    }
+    if(this.breakPointSubscription) {
+      this.breakPointSubscription.unsubscribe();
     }
   }
 }
